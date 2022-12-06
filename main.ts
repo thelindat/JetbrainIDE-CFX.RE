979946c3a85303d92bd0621e091e471bc379a696
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-12-06 22:42:11 +0000

    add native docs url getter

--- main.ts
+++ main.ts
@@ -24,6 +24,19 @@ export class Main {
         return "https://runtime.fivem.net/doc/natives.json";
     }
   };
+
+  /**
+   * @param gametype
+   */
+  private static getNativeDocsUrl = (gametype: GamesType): string => {
+    switch (gametype) {
+      case GamesType.RDR3:
+        return "https://alloc8or.re/rdr3/nativedb/?n=";
+      default:
+        return "https://docs.fivem.net/natives/?_";
+    }
+  };
+
   /**
    * Startup logicNom de la native fivem
    *
